34a27776ec73e7f9c004f31c02733932b59d578a
Kazushi Kawamura <wakaka2277@gmail.com>
2019-09-12 17:22:11 +0900

    Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Purescript Halogen Spago Starter
---
+++ src/Main.purs
@@ -0,0 +1,14 @@
+
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Button as Button
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI Button.component unit body
